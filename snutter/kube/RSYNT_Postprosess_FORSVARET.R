2ebc69a98bdcfdd7aaf5fa2be067aa8fedf079a4
Vegard <vegard.lysne@helsedir.no>
2025-02-14 13:06:31 +0100

    update forsvaret postprosess

--- snutter/kube/RSYNT_Postprosess_FORSVARET.R
+++ snutter/kube/RSYNT_Postprosess_FORSVARET.R
@@ -2,12 +2,11 @@
 # Author: Vegard Lysne
 # Updated: 2024.02.07
 
-# Delete data for AAlesund/Haram for years prior to 2020, as they are in large part provided as 1507 which is invalid from 2024 and coded to 1599. 
+# Delete data for AAlesund/Haram for years between 2018  and 2023, as they are in large part provided as 1507 which is invalid and coded to 1599. 
 
-KUBE[GEO %in% c("1508", "1580") &
-    as.numeric(substr(AAR, 6, 9)) < 2020,
-    `:=` (RATE = NA_real_,
-          SMR = NA_real_,
-          MEIS = NA_real_,
-          RATE.f = 1,
-          TELLER.f = 1)]
+KUBE[GEO %in% c("1508", "1580") & (AARl == 2018 | AARh == 2018 | AARl < 2018 & AARh > 2018), 
+     let(RATE = NA_real_,
+         SMR = NA_real_,
+         MEIS = NA_real_,
+         RATE.f = 1,
+         TELLER.f = 1)]
