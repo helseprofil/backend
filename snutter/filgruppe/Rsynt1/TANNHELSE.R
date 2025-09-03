67fb474b571dc2a0207bc523fa27d7350034de79
Vegard <vegard.lysne@helsedir.no>
2025-09-03 15:12:48 +0200

    edit rsynt1 tannhelse

--- snutter/filgruppe/Rsynt1/TANNHELSE.R
+++ snutter/filgruppe/Rsynt1/TANNHELSE.R
@@ -1,3 +1,3 @@
 # Korrigerer ALDERSkolonnen B slik at bare tallet hentes ut. 
-DF[, ALDER := sub(".*(\\d+)-.*", "\\1", B)]
+DF[, ALDER := sub(".*?(\\d+)-.*", "\\1", B)]
 DF[, B := trimws(gsub("[1-9][1-9]?-.ringer", "", B))]
