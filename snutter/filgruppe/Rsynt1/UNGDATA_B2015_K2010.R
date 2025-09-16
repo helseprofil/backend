2523c3738ff1a289c401a7504be95a42a740ef96
Vegard <vegard.lysne@helsedir.no>
2025-09-16 14:47:15 +0200

    rewrite ungdata rsynt 1 main

--- snutter/filgruppe/Rsynt1/UNGDATA_B2015_K2010.R
+++ snutter/filgruppe/Rsynt1/UNGDATA_B2015_K2010.R
@@ -1,3 +1,65 @@
+# Rsynt 1 for innlesing av vanlige ungdataindikatorer
+# Bydelstall fra 2015 og kommunetall fra 2010
+
+tab1 <- unique(DF$tab1_innles) # Kan bruke filedescription$TAB1, men do_special_handling må også kunne ta med filedescription før dette kan brukes
+minaar <- collapse::fmin(unique(DF$AAR))
+if(minaar > 2012) minaar <- 2012 # rektangulariser minst tilbake til 2012
+maxaar <- collapse::fmax(unique(DF$AAR))
+if(is.numeric(DF[[tab1]])) DF <- DF[get(tab1) < 98]
+data.table::setnames(DF, c("klasse", "kommune", "tidspunkt"), c("Klasse", "Kommune", "Tidspunkt"), skip_absent = T)
+DF[is.na(SOES), let(SOES = 99)]
+
+bydelcols <- grep("^Bydel", names(DF), ignore.case = TRUE, value = T)
+isbydel <- length(bydelcols) > 0
+keepcols <- c(tab1, "Kommune", "AAR", "Tidspunkt", "KJONN", "Klasse", "SOES", "vekt2020")
+
+if(isbydel){
+  keepcols_bydel <- c(intersect(keepcols, names(DF)), bydelcols)
+  DF <- DF[, .SD, .SDcols = keepcols_bydel]
+  DF[, let(nonmiss = rowSums(!is.na(.SD)),
+           miss = Reduce(`+`, lapply(.SD, function(x) x %in% c(98, 99)))), .SDcols = bydelcols]
+  DF <- DF[nonmiss == 1 & miss == 0 & !(Kommune %in% c(1601, 5001) & AAR == 2013)][, let(nonmiss = NULL, miss = NULL)]
+  DF[Kommune == 301 & AAR >= 2015, let(Kommune = get(grep("oslo", bydelcols, ignore.case = T, value = T)))]
+  DF[Kommune == 1103 & AAR >= 2015, let(Kommune = get(grep("stavanger", bydelcols, ignore.case = T, value = T)))]
+  DF[Kommune %in% c(1201, 4601) & AAR >= 2015, let(Kommune = get(grep("bergen", bydelcols, ignore.case = T, value = T)))]
+  DF[Kommune %in% c(1601, 5001) & AAR >= 2015, let(Kommune = get(grep("trondheim", bydelcols, ignore.case = T, value = T)))]
+  DF[, (bydelcols) := NULL]
+  if(!"vekt2020" %in% names(DF)) DF[, let(vekt2020 = 1)]
+}
+DF <- DF[, .SD, .SDcols = keepcols]
+allgeos <- unique(DF$Kommune)
+DF <- DF[!is.na(Klasse) & !is.na(KJONN)]
+
+DF[Tidspunkt == 99 & AAR > 2021, let(Tidspunkt = 1)]
+DF[Klasse < 98, let(klasse_6delt = paste0(Klasse + 7, "_", 2/Tidspunkt))]
+DF[Klasse >= 98, let(klasse_6delt = paste0(Klasse, "_", 2/Tidspunkt))]
+DF[, let(teller = 1)]
+DF[is.na(vekt2020), let(vekt2020 = 1)]
+
+dims <- c(tab1, "Kommune", "AAR", "KJONN", "klasse_6delt", "SOES")
+g <- collapse::GRP(DF, dims)
+DF <- collapse::add_vars(g[["groups"]], collapse::fsum(collapse::get_vars(DF, c("teller", "vekt2020")), g = g))
+DF[, let(exist = 1)]
+
+full <- list()
+full[["Kommune"]] <- data.table::data.table(Kommune = allgeos)
+full[["klasse_6delt"]] <- data.table::data.table(klasse_6delt = c("10_1", "10_2", "9_1", "9_2", "8_1", "8_2"))
+full[["AAR"]] <- data.table::data.table(AAR = minaar:maxaar)
+for(dim in setdiff(dims, c("Kommune", "klasse_6delt", "AAR"))){
+  full[[dim]] <- unique(DF[, .SD, .SDcols = dim])
+}  
+
+full <- do.call(expand.grid.dt, full)
+data.table::setcolorder(full, c("Kommune", "AAR", "KJONN", tab1, "klasse_6delt", "SOES"))
+DF <- collapse::join(full, DF, multiple = T, overid = 2, verbose = 0)
+DF[is.na(exist), let(teller = 0, vekt2020 = 0)][, let(exist = NULL)]
+
+# Lag nevner som sum(teller) i hvert strata og gjør alt til tekst
+DF[, let(nevner = collapse::fsum(teller)), by = setdiff(dims, tab1)]
+DF[, names(.SD) := lapply(.SD, as.character)]
+
+
+# Gamle kommentarer fra STATA
 # /*
 #   Forskjellen mellom filen(e) med gamle bydelstall og andre filer, er at bydels-
 #   variablene har byttet a) navn og b) innhold (har senere fått autentiske bydels-
@@ -48,123 +110,3 @@
 #   metoden gir uventet store problemer på pre_FGlagring.
 # 
 # ******************************************************************************/
-#   /*
-#   Rsynt1_Ungdata_felles_B_2015-_K_2010-_v01.do
-# Hardkodet GEO: Ja. De fire største byene + bydeler der.
-# Trøndelag og Vestland er tatt høyde for (funker med både nye og gml. komm.nr).
-# */
-#   
-#   pause on
-# set more off
-# local forsteUngdataAar=2012 // Bør være første Ungdata-år i Kommunehelsa (2012)
-# su AAR
-# local innevaerendeAar=`r(max)'
-# local origVar=tab1_innles 
-# 
-# * PRE-PROSESSERING. Omkode "."  i SOES til "99". Burde helst vært gjort i kodebok, 
-# 	*men den metoden gir uventet store problemer på pre_FGlagring (JM 17.1.2022) 
-# replace SOES=99 if SOES==.
-# 
-# drop if `origVar'>=98
-# capture rename klasse Klasse // flyttet denne linjen ett hakk opp
-# capture rename kommune Kommune
-# capture rename tidspunkt Tidspunkt
-# * FILTRERING, forskjell mellom kommune- og bydelsfil
-# capture su bydel_* // * Finne ut om dette er en KOMMUNE- eller BYDELsfil 
-# if _rc>0 { // hvis det er kommunefilen
-#   keep `origVar' Kommune AAR Tidspunkt KJONN Klasse SOES vekt2020
-# }
-# if _rc==0 { // hvis det er bydelsfilen
-# 	* Droppe eventuelle rene kommunetall (20.12.2021). Telle opp hvor mange av 
-# 	* "bydel_..."-variablene som ikke er missing. Skal være = 1, for alle 
-# 	* observasjoner skal kunne knyttes til en bydel.
-# 	tempvar Bydel_nonmiss
-# 	egen `Bydel_nonmiss'=rownonmiss(bydel*)
-# drop if `Bydel_nonmiss'==0 // drop hvis det er missing (bokstavelig talt) på
-# 	// alle bydelsvariablene. F.o.m. 2021 kodes det 'missing' i stedet for 98/99.
-# 	keep `origVar' Kommune AAR Tidspunkt KJONN Klasse bydel* SOES
-# drop if Ko==301 & (bydel_oslo==98 | bydel_oslo==99)
-# drop if Ko==1103 & (bydel_stavanger==98 | bydel_stavanger==99) 
-# capture drop if (Ko==1601 | Ko==5001) & (bydel_Trondheim==98 | bydel_Trondheim==99) //stor forbokstav
-# capture drop if (Ko==1601 | Ko==5001) & (bydel_trondheim==98 | bydel_trondheim==99) //liten forbokstav
-# drop if (Ko==1201 | Ko==4601) & 	(bydel_bergen==98 | bydel_bergen==99)
-# drop if Ko==(Ko==1601 | Ko==5001) & AAR==2013 // Kan ikke konverteres til de offisielle bydelene
-# replace Kommune=bydel_oslo if Ko==301 & AAR>=2015
-# replace Kommune=bydel_bergen if (Ko==1201 | Ko==4601) & AAR>=2015
-# capture replace Kommune=bydel_Trondheim if (Ko==1601 | Ko==5001) & AAR>=2015
-# capture replace Kommune=bydel_trondheim if (Ko==1601 | Ko==5001) & AAR>=2015
-# replace Kommune=bydel_stavanger if Ko==1103 & AAR>=2015	  // Storhaug mangler i 2016, pga. St Svithun mixup
-# assert Ko>30000
-# }
-# * KLASSE + SEMESTER (SEMESTER=<Tidspunkt>. 1=før sommeren, 2=etter sommeren)
-# replace Tidspunkt=1 if Tidspunkt==99 & AAR>=2021 // "99" er nytt i 2021 og skaper trøbbel (jørgen 14.des.2021)
-# gen klasse_6delt=strofreal(Klasse+7)+"_"+strofreal(2/Tidsp)
-# replace klasse_6delt=strofreal(Klasse)+"_"+strofreal(2/Tidsp) if Klasse==98 | Klasse==99 
-# 
-# * TELLER
-# gen teller=1 
-# capture gen vekt2020=1  // Ny i 2020! Legge inn <vekt2020> i bydelsfiler. 
-# replace vekt2020=1 if vekt2020==. // Ny i 2020! Fylle ut missingverdier i ekte vekt2020
-# collapse (sum) teller vekt2020, by(`origVar' Kommune AAR KJONN klasse_6delt SOES) 
-# * På dette stadiet: Hvis i en kommune/bydel, på et klassetrinn, for fx gutter
-# * det var 0 som svarte et av svaralternativene, så vil det på dette stadiet 
-# * mangle linje for denne kombinasjonen (mens det egentlig skulle ha vært en 
-# * linje med teller=0. Det er dette som rettes opp med neste fillin:
-# * 23.1.2016: Rektangularisere, ellers mister vi strata med teller 0
-# fillin `origVar' Kommune AAR KJONN klasse_6delt SOES
-#                                     replace teller =0 if teller==. & _f==1
-#                                     replace vekt2020 =0 if vekt2020==. & _f==1 // Ny des. 2020
-#                                     drop _f
-#                                     * NEVNER
-#                                     egen nevner = total(teller), by(Kommune AAR KJONN klasse_6delt SOES) // des. 2020: Kun fra u-vektede tellere. Har ikke VALer nok til å beregne vektet nevner her.
-#                                     * 23.1.2016: Fillin-linjen gir massevis av tomme strata, i år der komunen ikke 
-#                                     * har gjenniomført undersøkelsen. Dropper derfor hvis nevner 0
-#                                     drop if nevner==0  // disse settes trolig inn igjen nedenfor men skitt au
-#                                     
-#                                     *"FILLIN" av alder. Det kan være et problem at ikke alle "Alders"-verdiene er 
-#                                     * representert i en gitt innfil. Kræsjer ved stabling med andre innfiler til  
-#                                     * filgruppe. Derfor legges det her inn noen  linjer som fører til at alle  
-#                                     * aldersverdier er representert i alle filer
-#                                     sort Kommune AAR KJONN klasse_6delt SOES // unngå evt. missingverdier på linje 1
-#                                     foreach alder in 10_1 10_2 8_1 8_2 98_1 98_2 99_1 99_2 9_1 9_2 {
-#                                       local nylinje=_N+1
-#                                       set obs `nylinje'
-# 	replace klasse_6delt="`alder'" in `nylinje'
-# 	replace Ko=Ko[1] in `nylinje'
-# 	replace KJ=1 in `nylinje'
-# 	replace AAR=AAR[1] in `nylinje'
-# 	replace `origVar'=`origVar'[1] in `nylinje'
-# 	replace SOES=SOES[1] in `nylinje'
-# 	replace teller=0 in `nylinje'
-# 	replace nevner=0 in `nylinje'
-# 	replace vekt2020=0 in `nylinje'    // Ny i 2020
-# }
-# 
-# *"FILLIN" av aar. Det kan være et problem at ikke alle "års"-verdiene er 
-# * representert i en gitt innfil. Kræsjer ved stabling med andre innfiler til  
-# * filgruppe. DETTE ER NESTEN IKKE MULIG!!! Derfor legges det her inn noen    
-# * linjer som fører til at alleårsverdier er representert i alle filer
-# sort Kommune AAR KJONN klasse_6delt SOES // unngå evt. missingverdier på linje 1
-# forvalues aar = `forsteUngdataAar'/`innevaerendeAar' {
-# 	local nylinje=_N+1
-# 	set obs `nylinje'
-# 	replace klasse_6delt=klasse_6delt[1] in `nylinje'
-# 	replace Ko=0 in `nylinje'
-# 	replace KJ=KJ[1] in `nylinje'
-# 	replace AAR=`aar' in `nylinje'
-# 	replace `origVar'=`origVar'[1] in `nylinje'
-# 	replace SOES=SOES[1] in `nylinje'
-# 	replace teller=0 in `nylinje'
-# 	replace nevner=0 in `nylinje'
-# 	collapse (sum) teller nevner vekt2020, by(Kommune AAR KJONN klasse_6delt SOES `origVar')
-# 	fillin Kommune AAR KJONN klasse_6delt SOES `origVar'
-# 	replace teller=0 if teller==. & _fillin==1
-# 	replace nevner=0 if nevner==. & _fillin==1
-# 	replace vekt2020=0 if vekt2020==. & _fillin==1    // Ny i 2020
-# 	drop _fillin
-# }
-# * TOSTRING
-# tostring _all, replace force  
-# * RYDDING
-# capture drop __0* //temp-variabelen  
-# *******************************************************************************
