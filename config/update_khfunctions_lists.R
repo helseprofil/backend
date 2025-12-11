a88b09ea34d6ccf23dea0e4d26beb2fcbb3efa4f
Vegard <vegard.lysne@helsedir.no>
2025-12-11 12:57:14 +0100

    khfun list

---
+++ config/update_khfunctions_lists.R
@@ -0,0 +1,15 @@
+#' @title update_khfunctions_dimlist
+#' @description 
+#' Leser TAB1-3-kolonnene i ACCESS og lager en liste over alle potensielle dimensjonskolonner
+#' Brukes for å identifisere dimensjoner og verdier. Returnerer en liste som kan limes rett inn i `config-khfunctions.yml`
+#' @noRd
+update_khfunctions_dimlist <- function(){
+  con <- qualcontrol:::ConnectKHelsa()
+  on.exit(RODBC::odbcClose(con), add = TRUE)
+  date <- qualcontrol:::SQLdate(Sys.time())
+  standarddimensions <- c("GEOniv", "GEO", "FYLKE", "AAR", "AARl", "AARh", "ALDER", "ALDERl", "ALDERh", "KJONN", "UTDANN", "INNVKAT", "LANDBAK")
+  tabdimensions <- data.table::setDT(RODBC::sqlQuery(con, paste0("SELECT TAB1, TAB2, TAB3 FROM FILGRUPPER WHERE VERSJONFRA <=",date, "AND VERSJONTIL >", date)))
+  tabdimensions <- data.table::melt(tabdimensions, measure.vars = c("TAB1", "TAB2", "TAB3"))[!is.na(value), unique(value)]
+  out <- c(standarddimensions, sort(tabdimensions))
+  cat(paste("[", paste(out, collapse = ", "), "]", sep = ""))
+}
