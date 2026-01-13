3b96b2be96e8fcbdfcd93802433207735106f560
Vegard Lysne <vegard.lysne@helsedir.no>
2026-01-13 12:52:44 +0100

    oppdatere trangboddhetsnutt

--- snutter/kube/Rsynt_Postprosess/TRANGBODDHET.R
+++ snutter/kube/Rsynt_Postprosess/TRANGBODDHET.R
@@ -7,9 +7,7 @@
 # - > 10% har BODD == "uoppgitt" (sletter BODD == "trangt")
 
 cat("\n\nSTARTER RSYNT_POSTPROSESS, R-SNUTT\n")
-cat("\nIdentifiserer relevante GEO-koder\n")
-
-cat("\nSletter bydelsdata med >8% ukjent sumTELLER eller >5%-poeng forskjell i ukjent sumTELLER/sumNEVNER")
+cat("\n**** Sletter bydelsdata med >8% ukjent sumTELLER eller >5%-poeng forskjell i ukjent sumTELLER/sumNEVNER")
 keepcols <- c("GEO","AAR","ALDER","UTDANN","LANDBAK","INNVKAT","BODD","sumTELLER","sumNEVNER")
 deletestrata <- data.table::copy(KUBE)[BODD == "trangt" & (GEOniv == "B" | GEO %in% c("0301", "1103", "4601", "5001")), .SD, .SDcols = keepcols]
 
@@ -32,14 +30,28 @@ deletestrata[, DIFF := sumTELLER - sumNEVNER]
 
 bydims <- c("GEOKODE", "AAR", "ALDER", "UTDANN", "INNVKAT")
 deletestrata <- deletestrata[sumTELLER > 0.08 | (DIFF > 0.05 | DIFF < -0.05)][, .SD, .SDcols = bydims]
-delete <- collapse::join(deletestrata, deletebydel, on = "GEOKODE", multiple = TRUE, verbose = FALSE, overid = 2)[, let(GEOKODE = NULL, SLETT = 1)]
+delete <- collapse::join(deletestrata, deletebydel, on = "GEOKODE", multiple = TRUE, verbose = FALSE, overid = 2)
+delete[, let(GEOKODE = NULL, SLETT = 1)]
 bydims <- sub("GEOKODE", "GEO", bydims)
 delete <- delete[, .SD, .SDcols = c(bydims, "SLETT")]
 KUBE <- collapse::join(KUBE, delete, on = bydims, overid = 2, verbose = 0)
-KUBE[SLETT == 1, (c("TELLER.f", "RATE.f")) := 1][, SLETT := NULL]
+KUBE[spv_tmp == 0 & SLETT == 1, c("TELLER.f", "RATE.f", "spv_tmp") := 1]
+KUBE[, SLETT := NULL]
 
-cat("\nSletter tall for BODD=='trangt' for strata med > 10% BODD=='uoppgitt'")
+cat("\**** nSletter tall for BODD=='trangt' for strata med > 10% BODD=='uoppgitt'")
 delete <- KUBE[BODD == "uoppgitt" & sumTELLER/sumNEVNER > 0.10 & GEOniv %in% c("B", "K"), .SD, .SDcols = bydims]
 delete[, let(BODD = "trangt", SLETT = 1)]
 KUBE <- collapse::join(KUBE, delete, on = c(bydims, "BODD"), overid = 2, verbose = FALSE)
-KUBE[SLETT == 1, (c("TELLER.f", "RATE.f")) := 1][, SLETT := NULL]
+KUBE[spv_tmp == 0 & SLETT == 1, c("TELLER.f", "RATE.f", "spv_tmp") := 1]
+KUBE[, SLETT := NULL]
+
+# januar 2026: 
+# Etter utvidelse med utdann blir det tilfeller hvor UTDANN 1 og 4 får tall, men ikke de andre. 
+# For å unngå forvirring fjerner vi alle tall for UTDANN = 1-4 dersom minst to av disse er prikket. 
+if(length(unique(KUBE$UTDANN)) > 1){
+  cat("\n**** Sletter tall for undergrupper av UTDANN dersom minst 2 kategorier er prikket i et strata")
+  bydims <- c(setdiff(bydims, "UTDANN"), "BODD")
+  KUBE[, n_prikk := sum(spv_tmp > 0), by = bydims]
+  KUBE[n_prikk >= 2 & UTDANN %in% c(1,2,3,4) & spv_tmp == 0, c("TELLER.f", "RATE.f", "spv_tmp") := 1]
+  KUBE[, n_prikk := NULL]
+}
