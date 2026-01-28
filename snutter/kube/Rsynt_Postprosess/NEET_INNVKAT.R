101a797cdd38c60e6a5a32b245df0131eb7f1ee8
Vegard Lysne <vegard.lysne@helsedir.no>
2026-01-28 13:05:29 +0100

    oppdater snutt neet_innvkat

--- snutter/kube/Rsynt_Postprosess/NEET_INNVKAT.R
+++ snutter/kube/Rsynt_Postprosess/NEET_INNVKAT.R
@@ -8,81 +8,51 @@
 
 cat("\n\nSTARTER RSYNT_POSTPROSESS, R-SNUTT\n")
 cat("\nIdentifiserer relevante GEO-koder\n")
-
-kommunegeo <- c("0301", "1103", "4601", "5001")
-bydelsgeo <- grep(paste(paste0("^", kommunegeo), collapse = "|"), unique(KUBE[GEOniv == "B", GEO]), value = TRUE)
-bydelsgeo <- bydelsgeo[!bydelsgeo %in% c(grep("99$", bydelsgeo, value = TRUE), # ukjent bydel m? ut f?r beregning
-                                         "030116", "030117")] # Skal ikke vises ut, tas derfor ut av beregningen
-cat("\nKommuner:\n")
-paste(kommunegeo, collapse = ", ")
-cat("\nBydeler:\n")
-paste(bydelsgeo, collapse = ", ")
-
-# Lag subset av KUBE med relevante geokoder og BODD == trangt, lag KOMMUNE og GEONIV-kolonner
-cat("\nOppretter deletestrata\n")
-.deletestrata <- copy(KUBE)
-.deletestrata <- .deletestrata[GEO %in% c(kommunegeo, bydelsgeo), 
-                               .(GEO,AAR,KJONN,ALDER,UTDANN,LANDBAK,INNVKAT,sumTELLER,sumNEVNER)]
+geo_k <- c("0301", "1103", "4601", "5001")
+deletestrata <- KUBE[(GEO %in% geo_k | GEOniv == "B") & !grepl("99$", GEO) & !GEO %in% c( "030116", "030117"), 
+                     .(GEOniv, GEO,AAR,KJONN,ALDER,UTDANN,LANDBAK,INNVKAT,sumTELLER,sumNEVNER)]
 
 # Opprette .GEONIV og .GEOKODE
-.deletestrata[, `:=` (.GEONIV = ".BYDEL",
-                      .GEOKODE = character())]
-.deletestrata[nchar(GEO) == 4, .GEONIV := ".KOMMUNE"]
+deletestrata[, let(KOMMUNE = sub("(^\\d{4}).*", "\\1", GEO))][, GEO := NULL]
+# .deletestrata[, `:=` (.GEONIV = ".BYDEL",
+#                       .GEOKODE = character())]
+# .deletestrata[nchar(GEO) == 4, .GEONIV := ".KOMMUNE"]
 
-.deletestrata[grep("^0301", GEO), .GEOKODE := "^0301"]
-.deletestrata[grep("^1103", GEO), .GEOKODE := "^1103"]
-.deletestrata[grep("^4601", GEO), .GEOKODE := "^4601"]
-.deletestrata[grep("^5001", GEO), .GEOKODE := "^5001"]
+# .deletestrata[grep("^0301", GEO), .GEOKODE := "^0301"]
+# .deletestrata[grep("^1103", GEO), .GEOKODE := "^1103"]
+# .deletestrata[grep("^4601", GEO), .GEOKODE := "^4601"]
+# .deletestrata[grep("^5001", GEO), .GEOKODE := "^5001"]
 
 # Identifiser og filtrer ut komplette strata
 ## Prikking er ikke et problem da sumTELLER ikke er prikket med unntak av for 99-koder og bydeler i Tromso (5401..)
-bycols <- c(".GEOKODE", ".GEONIV", "AAR", "KJONN", "ALDER", "UTDANN", "LANDBAK", "INNVKAT")
+bycols <- c("GEOniv", "KOMMUNE", "AAR", "KJONN", "ALDER", "UTDANN", "LANDBAK", "INNVKAT")
 
 cat("\nFinner komplette strata og finner total sumTELLER og sumNEVNER \n")
-.deletestrata[, MISSING := sum(is.na(sumTELLER)), by = bycols]
-.deletestrata <- .deletestrata[MISSING == 0]
-.deletestrata <- .deletestrata[, lapply(.SD, sum, na.rm = T), .SDcols = c("sumTELLER", "sumNEVNER"), by = bycols]
-
-# Omstrukturer tabell, vis sum for Kommune og Bydel
+deletestrata <- deletestrata[, MISSING := sum(is.na(sumTELLER)), by = bycols][MISSING == 0]
 cat("\nOmstrukturerer tabell og beregner andelen ukjent bydel\n")
-.deletestrata <- melt(.deletestrata, measure.vars = c("sumTELLER", "sumNEVNER"), variable.name = "MALTALL", value.name = ".VALUE")
-.deletestrata <- dcast(.deletestrata, ... ~ .GEONIV, value.var = ".VALUE")
-.deletestrata[, .UKJENT := 1 - (.BYDEL / .KOMMUNE)]
+g <- collapse::GRP(deletestrata, bycols)
+deletestrata <- collapse::add_vars(g[["groups"]], 
+                                   collapse::fsum(collapse::get_vars(deletestrata, c("sumTELLER", "sumNEVNER")), g = g))
+# Omstrukturer tabell, vis sum for Kommune og Bydel
+deletestrata <- data.table::melt(deletestrata, measure.vars = c("sumTELLER", "sumNEVNER"), variable.name = "MALTALL", value.name = "VALUE")
+deletestrata <- data.table::dcast(deletestrata, ... ~ GEOniv, value.var = "VALUE")
+deletestrata[, UKJENT := 1 - (B/K)]
 
 cat("\nOmstrukturerer og beregner diff ukjent sumTELLER og sumNEVNER\n")
-.deletestrata <- dcast(.deletestrata, .GEOKODE + AAR + KJONN + ALDER + INNVKAT ~ MALTALL, value.var = ".UKJENT")
-.deletestrata[, .DIFF := sumTELLER - sumNEVNER]
+deletestrata <- data.table::dcast(deletestrata, KOMMUNE + AAR + KJONN + ALDER + INNVKAT ~ MALTALL, value.var = "UKJENT")
+deletestrata[, DIFF := sumTELLER - sumNEVNER]
 
 # Filtrer ut strata hvor ukjent bydel sumTELLER > 8 % eller differansen er > 5 %-poeng
 cat("\nFiltrerer ut rader med > 8 % ukjent sumTELLER eller > 5 %-poeng diff\n")
-.deletestrata <- .deletestrata[sumTELLER > 0.08 | (.DIFF > 0.05 | .DIFF < -0.05)]
-deletedbydel <<- copy(.deletestrata)
-.deletestrata <- .deletestrata[, .(.GEOKODE, AAR, KJONN, ALDER, INNVKAT)]
-cat("\nBydelstall for f?lgende strata slettes\n")
-cat("Se objektet `deletedbydel` i environment for oversikt over andel missing sumTELLER, sumNEVNER og diff\n")
-print(.deletestrata)
+deletestrata <- deletestrata[sumTELLER > 0.08 | abs(DIFF) > 0.05, .(KOMMUNE, AAR, KJONN, ALDER, INNVKAT)]
+cat("\nBydelstall for", deletestrata[, .N], "strata slettes\n")
 
+deletestrata[, SLETT := 1]
 # Loop gjennom identifiserte strata, slett bydelstall.
+KUBE[GEOniv == "B", KOMMUNE := sub("(^\\d{4}).*", "\\1", GEO)]
+KUBE <- collapse::join(KUBE, deletestrata, multiple = "T", on = c("KOMMUNE", "AAR", "KJONN", "ALDER", "INNVKAT"), overid = 2, verbose = F)
+cat("\nSletter", KUBE[spv_tmp == 0 & SLETT == 1, .N], "rader hvor bydel har > 8% ukjent sumTELLER eller > 5%-poeng absolutt diff mellom ukjent sumTELLER og sumNEVNER")
 
-cat("\nSletter bydelsdata med > 8% ukjent bydel sumTELLER > 5%-poeng absolutt diff mellom ukjent bydel sumTELLER og sumNEVNER")
-
-for (i in 1:nrow(.deletestrata)) {
-  KUBE[GEOniv == "B" & 
-         grepl(.deletestrata[[".GEOKODE"]][i], GEO) & 
-         AAR == .deletestrata[["AAR"]][i] & 
-         KJONN == .deletestrata[["KJONN"]][i] &
-         ALDER == .deletestrata[["ALDER"]][i] &
-         INNVKAT == .deletestrata[["INNVKAT"]][i],
-       `:=`  (TELLER = NA_real_,
-              RATE = NA_real_,
-              SMR = NA_real_,
-              MEIS = NA_real_,
-              RATE.n = NA_real_,
-              sumTELLER = NA_real_,
-              sumNEVNER = NA_real_,
-              TELLER.f = 1,
-              RATE.f = 1)]
-}
-
-# Remove unneedet objects
-rm(.deletestrata)
+flags <- c(grep("\\.f$", names(KUBE), value = T), "spv_tmp")
+KUBE[spv_tmp == 0 & SLETT == 1, (flags) := 1]
+KUBE[, SLETT := NULL]
