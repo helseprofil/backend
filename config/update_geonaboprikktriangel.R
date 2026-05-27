0f2422f1e3f1a1592e5a90b0f78f514170296b40
Vegard <vegard.lysne@helsedir.no>
2026-05-27 13:44:11 +0200

    oppdatere geotriangler, lks etter befolkning 2024

--- config/update_geonaboprikktriangel.R
+++ config/update_geonaboprikktriangel.R
@@ -5,47 +5,63 @@
 # data <- arrow::open_dataset("O:/Prosjekt/FHP/PRODUKSJON/PRODUKTER/FILGRUPPER/NYESTE/BEF_GKny_aar_geo") |> 
 #   dplyr::filter(AARl == 2025) |> dplyr::collect() |> data.table::as.data.table()
 
-update_geonaboprikk_triangel <- function(d = data, cpo = 2024){
+update_geonaboprikk_triangel <- function(refaar = 2024){
+  
+  folder <- "O:/Prosjekt/FHP/PRODUKSJON/PRODUKTER/KUBER/STATBANK/DATERT/parquet"
+  files <- list.files(folder, pattern = "^BEFOLK_GK_\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2}\\.parquet$")
+  
+  file <- file.path(folder, max(files))
+  d <- data.table::setDT(arrow::read_parquet(file))
   
   out <- list()
-  dt <- d[grepl(aar, AAR) & KJONN == 0 & ALDER == "0_120"][, .(GEO, TELLER)][order(-TELLER)]
-  dt[qualcontrol:::.popinfo, let(GEOniv = i.GEOniv), on = "GEO"][, GEO := as.character(GEO)]
-  dt[GEO != 0 & nchar(GEO) %in% c(1,3,5,7), let(GEO = paste0("0", GEO))]
+  d <- d[grepl(refaar, AAR) & KJONN == 0 & ALDER == "0_120"][, .(GEO, TELLER)][order(-TELLER)]
+  d[, GEOniv := data.table::fcase(as.numeric(GEO) == 0, "L",
+                                  as.numeric(GEO) <= 99, "F",
+                                  as.numeric(GEO) <= 9999, "K",
+                                  as.numeric(GEO) <= 999999, "B",
+                                  default = "V")]
+  # dt[qualcontrol:::.popinfo, let(GEOniv = i.GEOniv), on = "GEO"][, GEO := as.character(GEO)]
+  d[GEO != 0 & nchar(GEO) %in% c(1,3,5,7), let(GEO = paste0("0", GEO))]
   
-  fylke <- dt[GEOniv == "F"]
-  kommune <- dt[GEOniv == "K"]
-  bydel <- dt[GEOniv == "B"]
-  
-  out[["LF"]] <- paste0("{0,", paste0(fylke$GEO, collapse = ","), "}")
+  out[["LF"]] <- paste0("{0,", paste0(unique(d[GEOniv == "F"]$GEO), collapse = ","), "}")
   FK <- character()
-  for(f in fylke$GEO){
-    kommuner <- paste0(grep(paste0("^", f), kommune$GEO, value = T), collapse = ",")
-    FK <- paste0(FK, paste0("{",f,",", kommuner, "}"))
+  for(fylke in unique(d[GEOniv == "F"]$GEO)){
+    kommuner <- paste0(grep(paste0("^", fylke), unique(d[GEOniv == "K"]$GEO), value = T), collapse = ",")
+    FK <- paste0(FK, paste0("{",fylke,",", kommuner, "}"))
   }
   out[["FK"]] <- FK
 
-  byer <- c("0301", "1103", "4601", "5001")
+  bydeler <- d[GEOniv == "B", unique(GEO)]
+  bydelskommuner <- unique(substr(bydeler, 1, 4))
   KB <- character()
-  for(b in byer){
-    bydeler <- paste0(grep(paste0("^", b), bydel$GEO, value = T), collapse = ",")
-    KB <- paste0(KB, paste0("{",b,",", bydeler, "}"))
+  for(bydel in bydelskommuner){
+    bydeler <- paste0(grep(paste0("^", bydel), d[GEOniv == "B"]$GEO, value = T), collapse = ",")
+    KB <- paste0(KB, paste0("{",bydel,",", bydeler, "}"))
   }
   out[["KB"]] <- KB
   
+  LKS <- character()
+  soner <- d[GEOniv == "V", unique(GEO)]
+  overniv <-  unique(sub("00$", "", substr(soner, 1, 6)))
+  for(parent in overniv){
+    lks <- paste0(grep(paste0("^", parent), d[GEOniv == "V"]$GEO, value = T), collapse = ",")
+    LKS <- paste0(LKS, paste0("{",parent,",", lks, "}"))
+  }
+  out[["LKS"]] <- LKS
   return(out)
 }
 
-test_lks_triangel <- function(){
- con <- khfunctions:::connect_khelsa() 
- on.exit(RODBC::odbcCloseAll())
- lks <- data.table::setDT(RODBC::sqlQuery(con, "SELECT GEO FROM GEOKODER WHERE GEONIV='V'", as.is = T))
- lks[, overniv := sub("00$", "", substr(GEO, 1, 6))]
- 
- LKS <- character()
- for(top in unique(lks$overniv)){
-   lkskoder <- paste0(lks[overniv == top, unique(GEO)], collapse = ",")
-   LKS <- paste0(LKS, paste0("{", top, ",", lkskoder, "}"))
- }
- return(LKS)
-}
-
+# test_lks_triangel <- function(){
+#  con <- khfunctions:::connect_khelsa() 
+#  on.exit(RODBC::odbcCloseAll())
+#  lks <- data.table::setDT(RODBC::sqlQuery(con, "SELECT GEO FROM GEOKODER WHERE GEONIV='V'", as.is = T))
+#  lks[, overniv := sub("00$", "", substr(GEO, 1, 6))]
+#  
+#  LKS <- character()
+#  for(top in unique(lks$overniv)){
+#    lkskoder <- paste0(lks[overniv == top, unique(GEO)], collapse = ",")
+#    LKS <- paste0(LKS, paste0("{", top, ",", lkskoder, "}"))
+#  }
+#  return(LKS)
+# }
+# 
