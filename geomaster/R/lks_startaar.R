e8604662a94d3ec9116fd36df06e1efc48ee265c
Vegard <vegard.lysne@helsedir.no>
2026-06-04 12:43:46 +0200

    geotabeller brukerkode oppdatert

--- geomaster/R/lks_startaar.R
+++ geomaster/R/lks_startaar.R
@@ -1,4 +1,20 @@
-get_lks_startaar <- function(bef_datotag = "2026-05-27-12-21", max_endring = 0.15){
+get_lks_startaar <- function(bef_datotag = "2026-06-03-13-22", 
+                             max_endring = 0.15, 
+                             basepath = root,
+                             dbpath = khelsa,
+                             write = F){
+  
+  cat("\n Connecting to databases")
+  con <- DBI::dbConnect(
+    odbc::odbc(),
+    .connection_string = paste0(
+      "Driver={Microsoft Access Driver (*.mdb, *.accdb)};",
+      "DBQ=", file.path(basepath, dbpath), ";"
+    ),
+    encoding = "UTF-8"
+  )
+  on.exit(DBI::dbDisconnect(con), add = T)
+  
   file <- file.path("O:/Prosjekt/FHP/PRODUKSJON/PRODUKTER/KUBER/STATBANK/DATERT/parquet", 
                     paste0("BEFOLK_GK_", bef_datotag, ".parquet"))
   d <- data.table::setDT(arrow::read_parquet(file))
@@ -9,11 +25,39 @@ get_lks_startaar <- function(bef_datotag = "2026-05-27-12-21", max_endring = 0.1
   out[, lks_startaar := 0L]
 
   d[, endring := (TELLER / data.table::shift(TELLER, type = "lag")) - 1, by = GEO]
-  d2 <- d[abs(endring) > max_endring]
-  d2 <- d2[, maxaar := max(AAR), by = GEO][AAR == maxaar]
-  d2 <- d2[, startaar := as.integer(substr(AAR, 1, 4))][, .(GEO, startaar, endring)]
+  d <- d[abs(endring) > max_endring]
+  d <- d[, maxaar := max(AAR), by = GEO][AAR == maxaar]
+  d <- d[, startaar := as.integer(substr(AAR, 1, 4))][, .(GEO, startaar, endring)]
+  d[, endring := round(100*endring, 2)]
+  out[d, on = "GEO", let(lks_startaar = i.startaar, endring = i.endring)]
   
-  out[d2, on = "GEO", let(lks_startaar = i.startaar, endring = i.endring)]
+  data.table::setnames(out, "endring", "befvekst_prosent")
+  
+  if(write){
+    
+    # Ask for confirmation before writing
+    opts <- c("Overwrite", "Cancel")
+    answer <- utils::menu(choices = opts, 
+                          title = paste0("Whoops!! You are now replacing the table LKS_STARTAAR in:\n\n", 
+                                         basepath, dbpath, 
+                                         "\n\nPlease confirm or cancel:"))
+    
+    if(answer == 1){
+      cat("\nUpdating the LKS_STARTAAR table in KHELSA...\n")
+      DBI::dbWriteTable(conn = con,
+                        name = "LKS_STARTAAR",
+                        value = out,
+                        batch_rows = 1,
+                        overwrite = TRUE,
+                        append = FALSE)
+      cat(paste0("\nDONE! New table written to:\n", basepath, dbpath, "\n\n"))
+    } else {
+      cat(paste0("\nYou cancelled, and the table was not overwritten! Puh!\n"))
+    }
+  }
+  
+  
+  
   
   return(out)  
 }
