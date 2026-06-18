9ad6098d9b951db4e6a9c6de06c39cf9a614324f
Vegard <vegard.lysne@helsedir.no>
2026-06-18 10:21:56 +0200

    fiks 14-sekunderssnutt

--- snutter/kube/Rsynt_Postprosess/utvidet_tellernevnerprikking.R
+++ snutter/kube/Rsynt_Postprosess/utvidet_tellernevnerprikking.R
@@ -1,5 +1,8 @@
 flags <- c(grep("\\.f$", names(KUBE), value = T), "spv_tmp")
 
-idx <- which(KUBE[["spv_tmp"]] != 0 & (KUBE[["sumTELLER"]] <= 6 || KUBE[["sumNEVNER"]] <= 20))
-data.table::set(KUBE, i = idx, j = flags, value = 3L)
-data.table::set(KUBE, i = idx, j = "manuellprikket", value = 1L)
+idx <- which(KUBE[["spv_tmp"]] != 0 & (KUBE[["sumTELLER"]] <= 6 | KUBE[["sumNEVNER"]] <= 20))
+if(length(idx) > 0){
+  cat("\n** Prikker ytterligere", length(idx), "rader fordi sumTELLER <= 6 eller sumNEVNER <= 20\n** Disse radene får SPVFLAGG 3 og manuellprikket = 1")
+  data.table::set(KUBE, i = idx, j = flags, value = 3L)
+  data.table::set(KUBE, i = idx, j = "manuellprikket", value = 1L)
+}
