94f82ea98443ee7eebf03b5c4db189089b4e06d3
Vegard <vegard.lysne@helsedir.no>
2026-06-18 13:30:05 +0200

    bugfix utvidet teller og nevnerprikking

--- snutter/kube/Rsynt_Postprosess/TANNHELSE.R
+++ snutter/kube/Rsynt_Postprosess/TANNHELSE.R
@@ -3,5 +3,10 @@
 # det var missing på den ene (som deretter medfører at det aggregerte tallet blir flagget), settes alle implisitte
 # nuller til 0. Det vil si at tall som ikke finnes inn får SPVFLAGG = 3, som må ordnes her slik at flagget blir 2. 
 
-KUBE[AARl %in% 2020:2023 & GEO %in% c("31", "32", "33", "39", "40", "55", "56"),
-     let(TELLER.f = 1, RATE.f = 1)]
+flags <- c(grep("\\.f$", names(KUBE), value = T), "spv_tmp")
+
+idx <- which(KUBE[["spv_tmp"]] == 0 & KUBE[["AARl"]] %in% 2020:2023 & KUBE[["GEO"]] %in% c("31", "32", "33", "39", "40", "55", "56"))
+if(length(idx) > 0){
+  data.table::set(KUBE, i = idx, j = flags, value = 3L)
+  data.table::set(KUBE, i = idx, j = "manuellprikket", value = 1L)
+}
--- snutter/kube/Rsynt_Postprosess/utvidet_tellernevnerprikking.R
+++ snutter/kube/Rsynt_Postprosess/utvidet_tellernevnerprikking.R
@@ -1,6 +1,6 @@
 flags <- c(grep("\\.f$", names(KUBE), value = T), "spv_tmp")
 
-idx <- which(KUBE[["spv_tmp"]] != 0 & (KUBE[["sumTELLER"]] <= 6 | KUBE[["sumNEVNER"]] <= 20))
+idx <- which(KUBE[["spv_tmp"]] == 0 & (KUBE[["sumTELLER"]] <= 6 | KUBE[["sumNEVNER"]] <= 20))
 if(length(idx) > 0){
   cat("\n** Prikker ytterligere", length(idx), "rader fordi sumTELLER <= 6 eller sumNEVNER <= 20\n** Disse radene får SPVFLAGG 3 og manuellprikket = 1")
   data.table::set(KUBE, i = idx, j = flags, value = 3L)
