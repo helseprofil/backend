77547c72b0072c56065e7e9c730f18c2decb98ce
Vegard <vegard.lysne@helsedir.no>
2026-08-31 12:51:50 +0200

    legge til teller i rsynt pre fglagring

--- snutter/filgruppe/Rsynt_Pre_FGlagring/SYSVAK.R
+++ snutter/filgruppe/Rsynt_Pre_FGlagring/SYSVAK.R
@@ -2,3 +2,9 @@
 # Frem til 2021 fikk vi bare tall for KJONN == 2, mens fra 2022 har vi også tall for gutter
 # Disse blir flyttet til egen vaksine HPV_M, med KJONN == 0. 
 Filgruppe[TAB1 == "HPV" & KJONN == 2, let(KJONN = 0)]
+
+# Generere tellerkolonne fra nevner og vaksinasjonsgrad
+data.table::set(Filgruppe, j = "ANTVAKS", value = round(Filgruppe[["VAKSGRAD"]] * (Filgruppe[["DEKNINGSGRUNNLAG"]]/100), 0))
+data.table::set(Filgruppe, j = "ANTVAKS.f", value = pmax(Filgruppe[["VAKSGRAD.f"]], Filgruppe[["DEKNIGSGRUNNLAG.f"]], na.rm = T))
+data.table::set(Filgruppe, j = "ANTVAKS.a", value = pmax(Filgruppe[["VAKSGRAD.a"]], Filgruppe[["DEKNIGSGRUNNLAG.a"]], na.rm = T))
+                
