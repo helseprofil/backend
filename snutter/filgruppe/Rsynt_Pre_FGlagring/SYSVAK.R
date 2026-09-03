13d9e340725c274afc6cd432150aa88739d492e4
Vegard <vegard.lysne@helsedir.no>
2026-09-03 11:43:25 +0200

    bugfix sysvak snutt

--- snutter/filgruppe/Rsynt_Pre_FGlagring/SYSVAK.R
+++ snutter/filgruppe/Rsynt_Pre_FGlagring/SYSVAK.R
@@ -4,7 +4,6 @@
 Filgruppe[TAB1 == "HPV" & KJONN == 2, let(KJONN = 0)]
 
 # Generere tellerkolonne fra nevner og vaksinasjonsgrad
-data.table::set(Filgruppe, j = "ANTVAKS", value = round(Filgruppe[["VAKSGRAD"]] * (Filgruppe[["DEKNINGSGRUNNLAG"]]/100), 0))
-data.table::set(Filgruppe, j = "ANTVAKS.f", value = pmax(Filgruppe[["VAKSGRAD.f"]], Filgruppe[["DEKNIGSGRUNNLAG.f"]], na.rm = T))
-data.table::set(Filgruppe, j = "ANTVAKS.a", value = pmax(Filgruppe[["VAKSGRAD.a"]], Filgruppe[["DEKNIGSGRUNNLAG.a"]], na.rm = T))
-                
+Filgruppe[, let(ANTVAKS = round(VAKSGRAD * (DEKNINGSGRUNNLAG/100), 0),
+                ANTVAKS.f = pmax(VAKSGRAD.f, DEKNINGSGRUNNLAG.f, na.rm = T),
+                ANTVAKS.a = pmax(VAKSGRAD.a, DEKNINGSGRUNNLAG.a, na.rm = T))]
